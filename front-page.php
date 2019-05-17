3ba9f918d7aad04acb1d9b79de4ea0eca1590ee1
smlo1769 <smlo1769@student.ju.se>
2019-05-17 14:39:11 +0200

    css front page update
    asasddasd

--- front-page.php
+++ front-page.php
@@ -1,13 +1,16 @@
 <?php get_header(); ?>
-<div class="container">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="fpcontainer">
+<div class="herotxt">
+	<h1>WELCOME JONKOPING</H1>
+</div>
 <?php
 if(have_posts()) {
 	while(have_posts()) {
 		the_post();
 		the_content();
-	}	
+	}
 }
 
 get_footer();
 ?>
-
